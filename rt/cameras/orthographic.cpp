5890cf1cd583f1a2ce334651116387c924367318
Suhas-G <suhas.g96.sg@gmail.com>
2022-11-11 16:36:18 +0100

    fix: reverse only x

--- rt/cameras/orthographic.cpp
+++ rt/cameras/orthographic.cpp
@@ -5,12 +5,19 @@ namespace rt {
 
 OrthographicCamera::OrthographicCamera(const Point& center, const Vector& forward, const Vector& up, float scaleX, float scaleY): 
     center(center), forward(forward.normalize()), up(up.normalize()), scaleX(scaleX), scaleY(scaleY) {
-    this->left = rt::cross(up, forward).normalize();
-    this->up = rt::cross(forward, this->left);
+    this->left = rt::cross(this->up, this->forward).normalize();
+    this->up = rt::cross(this->forward, this->left);
 }
 
 Ray OrthographicCamera::getPrimaryRay(float x, float y) const {
-    return Ray(((((x * scaleX / 2) * left) + ((-y * scaleY / 2) * up)) + center), forward.normalize());
+    // -x , -y, forward
+    // x, y, forward
+    // x, y. -forward
+    // x, -y, forward
+    // -x, y, forward - produces upside down
+    // return Ray(((((-x * scaleX / 2) * left) + ((-y * scaleY / 2) * up)) + center), -forward.normalize());
+    // return Ray(((((-x * scaleX / 2) * left) + ((-y * scaleY / 2) * up)) + center), forward.normalize());
+    return Ray(((((-x * scaleX / 2) * left) + ((y * scaleY / 2) * up)) + center), forward.normalize());
 }
 
 }
