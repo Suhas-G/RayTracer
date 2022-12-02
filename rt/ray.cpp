b24f2aabb885625d3a307a57fb59a17bdf5aeff1
Suhas-G <suhas.g96.sg@gmail.com>
2022-12-02 12:00:18 +0100

    using signbit instead of comparison

--- rt/ray.cpp
+++ rt/ray.cpp
@@ -1,6 +1,7 @@
 #include <core/assert.h>
 #include <core/macros.h>
 #include <rt/ray.h>
+#include <cmath>
 
 namespace rt {
 
@@ -9,9 +10,9 @@ Ray::Ray(const Point& o, const Vector& d): o(o), d(d)
     rt_assert(d != Vector::rep(0.0f));
 
     invD = 1.0f / this->d.normalize();
-    dirIsNeg[0] = d.x < 0;
-    dirIsNeg[1] = d.y < 0;
-    dirIsNeg[2] = d.z < 0;
+    dirIsNeg[0] = std::signbit(d.x) ? 1 : 0;
+    dirIsNeg[1] = std::signbit(d.y) ? 1 : 0;
+    dirIsNeg[2] = std::signbit(d.z) ? 1 : 0;
 }
 
 Point Ray::getPoint(float distance) const {
